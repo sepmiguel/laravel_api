f2f8c3a3c1026502123f950a580cf904b9fa4abe
devJj <58999479+SSCFI-2019@users.noreply.github.com>
2024-03-18 22:17:32 +0800

    add delete entry

--- app/Http/Controllers/Api/V1/TaskController.php
+++ app/Http/Controllers/Api/V1/TaskController.php
@@ -79,6 +79,8 @@ class TaskController extends Controller
      */
     public function destroy(Task $task)
     {
-        //
+        $task->delete();
+
+        return response()->noContent();
     }
 }
