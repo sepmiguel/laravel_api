2001bbcce3228511e72aceb866b7d81e96783a01
devJj <58999479+SSCFI-2019@users.noreply.github.com>
2024-03-20 22:31:55 +0800

    SPa Authentication Login

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::prefix('auth')->group(function(){
+    Route::post('/login', LoginController::class);
 });
